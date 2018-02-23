63fd07bd73c84689b81117874d4fc7c1739bf4cc
george <a2255992@hotmail.com>
2018-02-23 14:55:54 +0800

    add marker on map, and show coordination infos. Add searchbar below map. But only layout.

--- app/src/main/java/free/com/rge/bus_app/MainActivity.java
+++ app/src/main/java/free/com/rge/bus_app/MainActivity.java
@@ -1,7 +1,12 @@
 package free.com.rge.bus_app;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -33,11 +38,27 @@ public class MainActivity extends AppCompatActivity {
                 goMap();
             }
         });
+
+        grantPermission();
     }
 
     private void goMap() {
         Intent intent = new Intent();
         intent.setClass(context, MapActivity.class);
         startActivity(intent);
+    }
+
+    private void grantPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+        } else {
+//            mMap.setMyLocationEnabled(true);
+            // Permission has already been granted
+        }
     }
 }
--- app/src/main/java/free/com/rge/bus_app/MapActivity.java
+++ app/src/main/java/free/com/rge/bus_app/MapActivity.java
@@ -1,22 +1,30 @@
 package free.com.rge.bus_app;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by rgegeo on 22/02/2018.
  */
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private Context context;
     private GoogleMap mMap;
     private UiSettings mUiSettings;
 
@@ -24,6 +32,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_layout);
+        context = this;
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -35,25 +44,33 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         mUiSettings = mMap.getUiSettings();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Map permission isn\'t granted", Toast.LENGTH_LONG).show();
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
 
-//        // Keep the UI Settings state in sync with the checkboxes.
-//        mUiSettings.setZoomControlsEnabled(isChecked(R.id.zoom_buttons_toggle));
-//        mUiSettings.setCompassEnabled(isChecked(R.id.compass_toggle));
-//        mUiSettings.setMyLocationButtonEnabled(isChecked(R.id.mylocationbutton_toggle));
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            // TODO: Consider calling
-//            //    ActivityCompat#requestPermissions
-//            // here to request the missing permissions, and then overriding
-//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-//            //                                          int[] grantResults)
-//            // to handle the case where the user grants the permission. See the documentation
-//            // for ActivityCompat#requestPermissions for more details.
-//            return;
-//        }
-//        mMap.setMyLocationEnabled(isChecked(R.id.mylocationlayer_toggle));
-//        mUiSettings.setScrollGesturesEnabled(isChecked(R.id.scroll_toggle));
-//        mUiSettings.setZoomGesturesEnabled(isChecked(R.id.zoom_gestures_toggle));
+//        mUiSettings.setZoomControlsEnabled(true);
+        mUiSettings.setCompassEnabled(true);
+        mUiSettings.setMyLocationButtonEnabled(true);
+        mUiSettings.setScrollGesturesEnabled(true);
+        mUiSettings.setZoomGesturesEnabled(true);
 //        mUiSettings.setTiltGesturesEnabled(isChecked(R.id.tilt_toggle));
 //        mUiSettings.setRotateGesturesEnabled(isChecked(R.id.rotate_toggle));
+
+        /** ya, addmarker code */
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                mMap.clear();
+                mMap.addMarker(new MarkerOptions().position(latLng).title(latLng.toString())).showInfoWindow();
+            }
+        });
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                Toast.makeText(context, "marker position: " + marker.getPosition().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
